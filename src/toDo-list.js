640783d857f2dd938843990a0b6d0ff7b0a8f21c
mt.louzada <matheuslouzadaa@gmail.com>
2024-10-28 12:30:25 -0300

    feat: create main folder

---
+++ src/toDo-list.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3001/api/todos';
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    const response = await axios.get(API_URL);
+    setTodos(response.data);
+  };
+
+  const addTodo = async () => {
+    if (newTodo.trim()) {
+      const response = await axios.post(API_URL, { title: newTodo });
+      setTodos([...todos, response.data]);
+      setNewTodo('');
+    }
+  };
+
+  const updateTodo = async (id, title, completed) => {
+    await axios.put(`${API_URL}/${id}`, { title, completed: !completed });
+    fetchTodos();
+  };
+
+  const deleteTodo = async (id) => {
+    await axios.delete(`${API_URL}/${id}`);
+    fetchTodos();
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Nova tarefa"
+      />
+      <button onClick={addTodo}>Adicionar</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              style={{
+                textDecoration: todo.completed ? 'line-through' : 'none',
+                cursor: 'pointer'
+              }}
+              onClick={() => updateTodo(todo.id, todo.title, todo.completed)}
+            >
+              {todo.title}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
