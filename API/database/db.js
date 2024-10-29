610700459301f779138af6b31a2f260d63158d46
mt.louzada <matheuslouzadaa@gmail.com>
2024-10-28 22:23:35 -0300

    feat: change table name

--- API/database/db.js
+++ API/database/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database(':memory:');
 
 db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS todos (
+  db.run(`CREATE TABLE IF NOT EXISTS tarefas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     completed BOOLEAN
