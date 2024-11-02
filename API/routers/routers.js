43ba29a9d5591243b3d8af11d68ebeb216e6dcde
mt.louzada <matheuslouzadaa@gmail.com>
2024-11-02 18:10:03 -0300

    :recycle: refactor: rename routers paths for batter clarity in Postman tests

--- API/routers/routers.js
+++ API/routers/routers.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db.js');
 
-router.post('/todos', (req, res) => {
+router.post('/newtask', (req, res) => {
     const { title } = req.body;
     db.run(`INSERT INTO todos (title, completed) VALUES (?, ?)`, [title, false], function(err) {
       if (err) return res.status(500).json({ error: err.message });
@@ -10,14 +10,14 @@ router.post('/todos', (req, res) => {
     });
   });
 
-  router.get('/todos', (req, res) => {
+  router.get('/alltask', (req, res) => {
     db.all(`SELECT * FROM todos`, [], (err, rows) => {
       if (err) return res.status(500).json({ error: err.message });
       res.json(rows);
     });
   });
 
-  router.put('/todos/:id', (req, res) => {
+  router.put('/updatetask/:id', (req, res) => {
     const { id } = req.params;
     const { title, completed } = req.body;
     db.run(`UPDATE todos SET title = ?, completed = ? WHERE id = ?`, [title, completed, id], function(err) {
@@ -26,7 +26,7 @@ router.post('/todos', (req, res) => {
     });
   });
 
-  router.delete('/todos/:id', (req, res) => {
+  router.delete('/deletetask/:id', (req, res) => {
     const { id } = req.params;
     db.run(`DELETE FROM todos WHERE id = ?`, id, function(err) {
       if (err) return res.status(500).json({ error: err.message });
