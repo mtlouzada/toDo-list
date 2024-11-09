7c7b003a05da0a5f71ab6e455574a64dc818d9dc
mt.louzada <matheuslouzadaa@gmail.com>
2024-11-08 23:54:54 -0300

    :truck: chore: adjust database route in router folder

--- API/routers/routers.js
+++ API/routers/routers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../database/db.js');
+const db = require('../database/dbConnection.js');
 
 router.post('/newtask', (req, res) => {
     const { title } = req.body;
